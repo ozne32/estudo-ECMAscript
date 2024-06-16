1b27ea0ec699eb6119a7017927f048c274dd2989
ozne32 <enzorcc.mf3rs@gmail.com>
2024-06-15 21:52:29 -0300

    novo jeito de fazer um export asíncrono

--- imports/lib.mjs
+++ imports/lib.mjs
@@ -4,4 +4,27 @@ export const saudacao = ()=>{
     let data = new Date().toLocaleDateString()
     return console.log('oi, tudo certo ? meu querido:: ', data)
 }
+// aqui é só um exmeplo para colocar como coisas mudaram no ES13
+// ao ínves de fazer
+export async function aguardarPromessa(){
+    function fetchDados(){
+        return new Promise(resolve=>{
+            setTimeout(()=>{
+                resolve('acabou')
+            }, 2000)
+        })
+    }
+    const dados = await fetchDados()
+}
+// agr dá para fazer de uma mamenria bem mais enxuta, semprecisar colocando uma função assincrona 
+// aí dps outra função para aí ter  a promisse, e então esperar ela para conseguir retornar os dados
+export async function novoJeito(){
+    return new Promise(resolve=>{
+        setTimeout(()=>{
+            resolve('foi')
+        }, 3000)
+    })
+}
+const dados = await novoJeito()
+console.log(dados)
 
